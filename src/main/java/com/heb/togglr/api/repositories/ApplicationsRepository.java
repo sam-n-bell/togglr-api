0c36dd4f214c037581418f130ee21b524a21ab86
sambell <bell.sam@Outlook.com>
2020-02-19 19:11:06 -0600

    search query methods in featurerepo and keysrepo

--- src/main/java/com/heb/togglr/api/repositories/ApplicationsRepository.java
+++ src/main/java/com/heb/togglr/api/repositories/ApplicationsRepository.java
@@ -1,22 +1,11 @@
 package com.heb.togglr.api.repositories;
 
 import com.heb.togglr.api.entities.AppEntity;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface ApplicationsRepository extends CrudRepository<AppEntity, Integer> {
 
     public List<AppEntity> findAllByAdminsById_Id(String id);
-
-
-    public AppEntity findById(@RequestParam int id, Sort sort);
-
-    // Got Sort parameter idea from: https://stackoverflow.com/questions/53330135/spring-data-rest-findby-not-sorting
-    // Postman url: {{server}}/togglr-api/appEntities/search/findById?id=1&projection=includeSubObjects&sort=featuresById.descr,asc
-    // Tried a couple of different sort entity name formats: featuresById_descr, FeatureEntity.descr, FeatureEntity_descr, etc
-
-
 }
