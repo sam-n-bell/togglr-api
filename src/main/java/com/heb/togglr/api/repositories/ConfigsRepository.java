c004124eb1e308d4b1308d01699c19783c12e104
sambell <bell.sam@Outlook.com>
2020-02-25 12:51:25 -0600

    search for configs by appid and featureid

--- src/main/java/com/heb/togglr/api/repositories/ConfigsRepository.java
+++ src/main/java/com/heb/togglr/api/repositories/ConfigsRepository.java
@@ -1,9 +1,16 @@
 package com.heb.togglr.api.repositories;
 
+import com.heb.togglr.api.entities.KeysEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import com.heb.togglr.api.entities.ConfigsEntity;
 import com.heb.togglr.api.entities.ConfigsEntityPK;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface ConfigsRepository extends CrudRepository<ConfigsEntity, ConfigsEntityPK> {
+
+    List<ConfigsEntity> findByAppIdAndFeatureId(@RequestParam("appId") int appId, @RequestParam("featureId") int featureId);
+
 }
