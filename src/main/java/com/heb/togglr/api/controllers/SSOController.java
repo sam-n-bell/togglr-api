5d80aa546864a94e20300c8e8a7c7af09c4a3e15
Shikha <shikha.singh@utexas.edu>
2020-04-01 23:22:00 -0500

    sso changes

--- src/main/java/com/heb/togglr/api/controllers/SSOController.java
+++ src/main/java/com/heb/togglr/api/controllers/SSOController.java
@@ -2,16 +2,22 @@ package com.heb.togglr.api.controllers;
 
 import javassist.tools.web.BadHttpRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class SSOController {
+//    @Value("${spring.security.oauth2.client.registration.github.redirectUri}")
+//    private String ssourl;
 
     @RequestMapping(method = RequestMethod.GET, value = "/ssologin")
     @ResponseBody
-    public String ssologin() {
-return ("${heb.security.oauth2.client.registration.github.redirectUri}");
+    public ResponseEntity<?> ssologin() {
+//        System.out.println(System.getenv("SSO_URL"));
+        System.out.println("Debugging sso url");
+//        System.out.println(ssourl);
+//        return ResponseEntity.ok(ssourl);
+        return null;
     }
 }
