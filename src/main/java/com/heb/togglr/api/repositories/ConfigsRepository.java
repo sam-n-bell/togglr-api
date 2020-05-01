13a82a5abed419d3b587408ba2c4a808f28deb89
sambell <bell.sam@Outlook.com>
2020-05-01 16:14:46 -0500

    removed requestparams from repository search function

--- src/main/java/com/heb/togglr/api/repositories/ConfigsRepository.java
+++ src/main/java/com/heb/togglr/api/repositories/ConfigsRepository.java
@@ -1,16 +1,13 @@
 package com.heb.togglr.api.repositories;
 
-import com.heb.togglr.api.entities.KeysEntity;
-import org.springframework.data.repository.CrudRepository;
-
 import com.heb.togglr.api.entities.ConfigsEntity;
 import com.heb.togglr.api.entities.ConfigsEntityPK;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ConfigsRepository extends CrudRepository<ConfigsEntity, ConfigsEntityPK> {
 
-    List<ConfigsEntity> findByAppIdAndFeatureId(@RequestParam("appId") int appId, @RequestParam("featureId") int featureId);
+    List<ConfigsEntity> findByAppIdAndFeatureId(int appId, int featureId);
 
 }
